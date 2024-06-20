2838d5d4ffafa06be8448287ce6f6dce32f5a73a
Reval Govender <revalgovender>
2024-06-20 10:32:41 +0100

    update

--- main.py
+++ main.py
@@ -50,57 +50,32 @@ developers = [
 ]
 
 
-def add_matching_id_to_developer(candidates: list, developers: list) -> dict:
-    """
-    # Match Rule 1 - Exact match of the email addresses
-    """
-    # Key candidates by email
+def add_matching_id_to_developer(candidates: list, developers: list) -> list:
+    # Create dictionaries with keys for matching.
     candidates_by_email = {}
+    candidates_by_name_company = {}
     for candidate in candidates:
         candidates_by_email[candidate["email"]] = candidate
 
-    # Key developer by email
-    developers_by_email = {}
-    for developer in developers:
-        developers_by_email[developer["email"]] = developer
-
-    # Add candidate id to developer
-    for developer in developers:
-        # Validation
-        if not developer["email"]:
+        if not candidate["name"] or not candidate["company"]:
             continue
 
-        # Set candidate id to developer
+        company_name_key = f"{candidate['name']}_{candidate['company']}"
+        candidates_by_name_company[company_name_key] = candidate
+
+    # Add id to developer
+    for developer in developers:
         if developer["email"] in candidates_by_email:
             developer["id"] = candidates_by_email[developer["email"]]["id"]
             continue
 
-    """"
-    Match Rule 2 - Exact match of the name and company
-    """
-    # Key candidate by name_company
-    candidates_by_name_company = {}
-    for candidate in candidates:
-        candidates_by_name_company[candidate["name"] + "_" + candidate["company"]] = candidate
+        company_name_key = f"{developer['name']}_{developer['company']}"
+        if company_name_key in candidates_by_name_company:
+            developer["id"] = candidates_by_name_company[company_name_key]["id"]
 
-    # Key developer by name_company
-    developers_by_name_company = {}
-    for developer in developers:
-        developers_by_name_company[developer["name"] + "_" + developer["company"]] = developer
-
-    for developer in developers:
-        # Validation
-        if not developer["name"] or not developer["company"]:
-            continue
-
-        # Set candidate id to developer
-        if developer["name"] + "_" + developer["company"] in candidates_by_name_company:
-            developer["id"] = candidates_by_name_company[developer["name"] + "_" + developer["company"]]["id"]
-
-    return developers_by_email
+    return developers
 
 
 if __name__ == "__main__":
     matched_people = add_matching_id_to_developer(candidates, developers)
-    json_matched_people = json.dumps(matched_people, indent=4)
-    print(json_matched_people)
+    print(json.dumps(matched_people, indent=4))
